bb53377060281b5938d66c5fd5bb41c882e1e241
Sachin Gupta <sachin.aut@gmail.com>
2019-12-14 15:59:34 +0530

    ADD: File where file based environment variables can be placed for `source ./scripts/env-init.sh`

---
+++ scripts/env-init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+##
+## 1. This file is used to export enviornment variables not present in travis dashboard
+## - Must not have any secrets, better put them on travis dashbord or in .travi.yml using `travis encrypt`
+## - Advantage is that file remains in source control and can be used with any CI wihtout dashboard
+## - Just putting variables used in current travis dashboard for help (as comments)
+## - Advantage of putting them outside on dashbords is that your actual code can remain open source
+##   without you loosing your passwords
+##
+## - If you don't use export variables like `VAR=VALUE`, variables becomes local and lost once script
+##   caller is returned (Yuk)
+##
+## 2. This file also export a sample hello_world() method to let you know that things are working
+
+# Travis Dashboard Variables: F
+# export DKRHUB_USER="Dockerhub username here"
+# export DKRHUB_PASS="Dockerhub password here"
+# export DKRHUB_PROJ="Name of dockerhub project"
+# export DKRHUB_REPO="${DKRHUB_USER}/${DKRHUB_PROJ}""
+# export AUTH_EMAIL="Email of author for notifications"
+
+export HELLO_VARIABLE="! SACn Welcomes You [EnvVar]!"
